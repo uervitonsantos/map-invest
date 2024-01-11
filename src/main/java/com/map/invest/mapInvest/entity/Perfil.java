9faf5979bf9144626d84418708e0eb8bd0e5a3d9
uervitonsantos <uerviton@gmail.com>
2024-01-11 15:52:00 -0300

    commit de ajuste dos relacionamentos das tabelas

--- src/main/java/com/map/invest/mapInvest/entity/Perfil.java
+++ src/main/java/com/map/invest/mapInvest/entity/Perfil.java
@@ -1,10 +1,18 @@
 package com.map.invest.mapInvest.entity;
 
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 import java.util.Objects;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
 @Entity
 @Table(name = "PERFIL")
 public class Perfil implements MapInvestEntity {
@@ -28,59 +36,5 @@ public class Perfil implements MapInvestEntity {
             inverseJoinColumns = @JoinColumn(name = "PERMISSAO_TELA_ID", referencedColumnName = "PERMISSAOTELA_ID", insertable = false, updatable = false))
     private List<PermissaoTela> permissaoTelas;
 
-    public Perfil() {
-    }
 
-    public Long getPerfilID() {
-        return perfilID;
-    }
-
-    public void setPerfilID(Long perfilID) {
-        this.perfilID = perfilID;
-    }
-
-    public String getCodPerfil() {
-        return codPerfil;
-    }
-
-    public void setCodPerfil(String codPerfil) {
-        this.codPerfil = codPerfil;
-    }
-
-    public String getNomePerfil() {
-        return nomePerfil;
-    }
-
-    public void setNomePerfil(String nomePerfil) {
-        this.nomePerfil = nomePerfil;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public List<PermissaoTela> getPermissaoTelas() {
-        return permissaoTelas;
-    }
-
-    public void setPermissaoTelas(List<PermissaoTela> permissaoTelas) {
-        this.permissaoTelas = permissaoTelas;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Perfil perfil = (Perfil) o;
-        return Objects.equals(perfilID, perfil.perfilID) && Objects.equals(codPerfil, perfil.codPerfil) && Objects.equals(nomePerfil, perfil.nomePerfil) && Objects.equals(descricao, perfil.descricao) && Objects.equals(permissaoTelas, perfil.permissaoTelas);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(perfilID, codPerfil, nomePerfil, descricao, permissaoTelas);
-    }
 }
